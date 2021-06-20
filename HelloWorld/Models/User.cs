e4e3e2ba2d4c6c191aeb6399890083fb7528c022
ESW <adesuyiayo@rocketmail.com>
2021-06-20 13:51:36 +0100

    Add project files.

---
+++ HelloWorld/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Models
+{
+    class User
+    {
+
+        public string firstName;
+        public string lastName;
+        public int age;
+        public float height;
+        public float weight;
+
+        public void IntroduceUser()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName + "\nI am " + age + " years old\nI am " + height + "ft tall and " + weight + "kg in weight.");
+        }
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var numberStart = 2;
+            var total = 20.76f;
+            var character = 'A';
+            var name = "Ayodeji";
+            var isWorking = true;
+
+
+            Console.WriteLine(number);
+            Console.WriteLine(numberStart);
+            Console.WriteLine(total);
+            Console.WriteLine(character);
+            Console.WriteLine(name);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
